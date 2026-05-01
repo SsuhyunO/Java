e0b69c089e63bed73490b1444d2dadec1220e04f
osh7313240 <osh7313240@naver.com>
2026-05-01 14:57:53 +0900

    add Ch01

---
+++ Ch01/src/sub1/Hello.java
@@ -0,0 +1,21 @@
+package sub1;
+
+/*
+ * 
+ * 날짜 : 2026/04/27
+ * 이름 : 오수현
+ * 내용 : 1장 Java 개발환경 구축 실습
+ * 
+ */
+
+// Ctrl + : 확대, Ctrl - : 축소
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		// 프로그래밍 입문 문자열
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+		
+	}  
+}
