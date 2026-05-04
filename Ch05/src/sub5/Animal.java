d2d305f0dc2bebe03779aa0d15055f164e2dd79f
osh7313240 <osh7313240@naver.com>
2026-05-04 11:44:27 +0900

    update CH05

---
+++ Ch05/src/sub5/Animal.java
@@ -0,0 +1,7 @@
+package sub5;
+
+public class Animal {
+	protected void sound() {
+		System.out.println("Animal Sound!");
+	}
+}
---
+++ Ch05/src/sub5/Dog.java
@@ -0,0 +1,8 @@
+package sub5;
+
+public class Dog extends Animal{
+	@Override
+	protected void sound() {
+		System.out.println("멍멍!");
+	}
+}
